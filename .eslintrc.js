f30489019d2c3bbbc220f46c8ae1a4536fcb8527
Jeff Pettis <jeffrey.pettis@gmail.com>
2023-05-20 16:02:12 -0700

    configured cypress e2e testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,12 @@ module.exports = {
       version: 'detect'
     }
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:cypress/recommended'
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
